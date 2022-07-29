ab1e1da80d97bbcece35bcb1fcb3bb49d632b882
nicks <nickson_yzj@hotmail.com>
2022-07-29 12:48:17 +0800

    updated

---
+++ app/src/main/java/sg/edu/rp/c346/id21022186/mymovies_showmovies/ShowMovies.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id21022186.mymovies_showmovies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowMovies extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_movies);
+    }
+}
